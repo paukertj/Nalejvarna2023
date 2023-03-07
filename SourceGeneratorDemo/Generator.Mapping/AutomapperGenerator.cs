995e35cc690567dc9f23845908e0bbe4ed65b664
Jiri Paukert <jiri@paukert.org>
2023-03-07 23:11:42 +0100

    A few things for project setup

---
+++ SourceGeneratorDemo/Generator.Mapping/AutomapperGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Diagnostics;
+
+namespace SourceGeneratorDemo.Generator.Mapping
+{
+    [Generator]
+    public class AutomapperGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+           // Debugger.Launch();
+        }
+    }
+}
